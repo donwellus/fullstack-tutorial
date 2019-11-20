312be1f1c194642256011f10c7460121e1734c42
Daniel Tre <daniel.tre@gmail.com>
2019-11-20 15:50:37 -0300

    Added resolvers to the Apollo context

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,11 +2,13 @@
 
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const resolvers = require('./resolvers');
 
 const LaunchAPI = require("./datasources/launch");
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI()
   })
