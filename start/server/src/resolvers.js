c219cc89b630b6d2f9ae20630473599e66d41876
Daniel Tre <daniel.tre@gmail.com>
2019-11-20 15:50:09 -0300

    Created Query resolvers for Launch

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  Query: {
+    launches: (_, __, { dataSources }) =>
+      dataSources.launchAPI.getAllLaunches(),
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id })
+  }
+};
