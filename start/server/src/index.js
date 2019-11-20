5c108860cedc04ea367c452a56f319aca6cbfa96
Daniel Tre <daniel.tre@gmail.com>
2019-11-20 15:42:02 -0300

    Added Launch datasource in the Apollo context

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,7 +3,14 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require("./datasources/launch");
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI()
+  })
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
