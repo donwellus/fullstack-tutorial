c7acf82c1eefd52e424d733ba4e28b3eacdf4ac4
Daniel Tre <daniel.tre@gmail.com>
2019-11-20 16:18:50 -0300

    Added pagination to Lanuches

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,9 +1,26 @@
 "use strict";
 
+const { paginateResults } = require("./utils");
+
 module.exports = {
   Query: {
-    launches: (_, __, { dataSources }) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
+    },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id })
   }
